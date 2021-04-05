d862d2c534b22b50c4bac110702fe909a1878625
mrYegor <mr.yegor@yahoo.com>
2021-04-05 12:26:00 +0300

    graphs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as CanvasJS from './canvas.min';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'intsite-graph-app';
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    const dataPoints = [];
+    let y = 0;
+    for (let i = 0; i < 1000; i++) {
+      y += Math.round(5 + Math.random() * (-5 - 5));
+      dataPoints.push({ y });
+    }
+    const chart = new CanvasJS.Chart('chartContainer', {
+      zoomEnabled: true,
+      animationEnabled: true,
+      exportEnabled: true,
+      title: {
+        text: 'Random 1000 Points',
+      },
+      data: [
+        {
+          type: 'line',
+          dataPoints,
+        },
+      ],
+    });
+
+    chart.render();
+  }
 }
